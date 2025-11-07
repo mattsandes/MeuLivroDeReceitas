ec8e6b1217dcbb58682cf5ce737cdc547b41466b
Mateus Sandes <mateussandes@hotmail.com.br>
2025-11-07 04:14:20 -0300

    Adicionado a regra de negocio no controller

--- src/Backend/MyRecipeBook.API/Controllers/UserController.cs
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Application.UseCases.User.Register;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
 
@@ -12,6 +13,10 @@ public class UserController : Controller
     [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
     public IActionResult Register(RequestRegisterUserJson request)
     {
-        return Created();
+        var usecase = new RegisterUserUseCase();
+
+        var result = usecase.Execute(request);
+        
+        return Created(string.Empty, result);
     }
 }
